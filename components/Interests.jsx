48ad8c9f82f341e8aa6d4dcdecc5fbc323e68cbb
Zach Behnke <ztbehnke62@gmail.com>
2020-05-25 11:22:27 -0700

    Updates github icon.

--- components/Interests.jsx
+++ components/Interests.jsx
@@ -7,7 +7,7 @@ class Interests extends React.Component {
       <div className="interests-section" id="interests-section">
         <h2 className="section-header">Interests</h2>
         <div className="pure-g">
-          <div className="pure-u-1-2 pure-u-md-1-2 pure-u-lg-1-3"><InterestBox name="Github" iconClass="fa fa-github fa-5x" aria-hidden="true" link="https://github.com/Mechdriver"/></div> {/*Github*/}
+          <div className="pure-u-1-2 pure-u-md-1-2 pure-u-lg-1-3"><InterestBox name="Github" iconClass="fab fa-github fa-5x" aria-hidden="true" link="https://github.com/Mechdriver"/></div> {/*Github*/}
           <div className="pure-u-1-2 pure-u-md-1-2 pure-u-lg-1-3"><InterestBox name="Steam" iconClass="fa fa-steam fa-5x" aria-hidden="true" link="http://steamcommunity.com/id/mechdriver/"/></div> {/*Steam Account*/}
           <div className="pure-u-1-2 pure-u-md-1-2 pure-u-lg-1-3"><InterestBox name="PokéDex" iconClass="fa fa-paw fa-5x" aria-hidden="true" link="https://pokedextracker.com/u/zbehnke/living-dex"/></div> {/*Pokemon Living Dex*/}
           <div className="pure-u-1-2 pure-u-md-1-2 pure-u-lg-1-3"><InterestBox name="LinkedIn" iconClass="fa fa-linkedin-square fa-5x" aria-hidden="true" link="https://www.linkedin.com/in/zachbehnke/"/></div> {/*LinkedIn*/}
