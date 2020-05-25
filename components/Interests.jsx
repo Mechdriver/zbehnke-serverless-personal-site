bfc3792ab743539204f76834d3b3cfc99d2e3a36
Zach Behnke <ztbehnke62@gmail.com>
2020-05-25 11:53:25 -0700

    Updates icons.

--- components/Interests.jsx
+++ components/Interests.jsx
@@ -8,11 +8,11 @@ class Interests extends React.Component {
         <h2 className="section-header">Interests</h2>
         <div className="pure-g">
           <div className="pure-u-1-2 pure-u-md-1-2 pure-u-lg-1-3"><InterestBox name="Github" iconClass="fab fa-github fa-5x" aria-hidden="true" link="https://github.com/Mechdriver"/></div> {/*Github*/}
-          <div className="pure-u-1-2 pure-u-md-1-2 pure-u-lg-1-3"><InterestBox name="Steam" iconClass="fa fa-steam fa-5x" aria-hidden="true" link="http://steamcommunity.com/id/mechdriver/"/></div> {/*Steam Account*/}
-          <div className="pure-u-1-2 pure-u-md-1-2 pure-u-lg-1-3"><InterestBox name="PokéDex" iconClass="fa fa-paw fa-5x" aria-hidden="true" link="https://pokedextracker.com/u/zbehnke/living-dex"/></div> {/*Pokemon Living Dex*/}
-          <div className="pure-u-1-2 pure-u-md-1-2 pure-u-lg-1-3"><InterestBox name="LinkedIn" iconClass="fa fa-linkedin-square fa-5x" aria-hidden="true" link="https://www.linkedin.com/in/zachbehnke/"/></div> {/*LinkedIn*/}
-          <div className="pure-u-1-2 pure-u-md-1-2 pure-u-lg-1-3"><InterestBox name="Strength" iconClass="fa fa-user fa-5x" aria-hidden="true" link="https://symmetricstrength.com/lifter/mechdriver"/></div> {/*Symmetric Strength*/}
-          <div className="pure-u-1-2 pure-u-md-1-2 pure-u-lg-1-3"><InterestBox name="Spotify" iconClass="fa fa-spotify fa-5x" aria-hidden="true" link="https://open.spotify.com/user/mechdriver"/></div> {/*Spotify Account*/}
+          <div className="pure-u-1-2 pure-u-md-1-2 pure-u-lg-1-3"><InterestBox name="Steam" iconClass="fab fa-steam fa-5x" aria-hidden="true" link="http://steamcommunity.com/id/mechdriver/"/></div> {/*Steam Account*/}
+          <div className="pure-u-1-2 pure-u-md-1-2 pure-u-lg-1-3"><InterestBox name="PokéDex" iconClass="fas fa-paw fa-5x" aria-hidden="true" link="https://pokedextracker.com/u/zbehnke/living-dex"/></div> {/*Pokemon Living Dex*/}
+          <div className="pure-u-1-2 pure-u-md-1-2 pure-u-lg-1-3"><InterestBox name="LinkedIn" iconClass="fab fa-linkedin fa-5x" aria-hidden="true" link="https://www.linkedin.com/in/zachbehnke/"/></div> {/*LinkedIn*/}
+          <div className="pure-u-1-2 pure-u-md-1-2 pure-u-lg-1-3"><InterestBox name="Strength" iconClass="fas fa-dumbbell fa-5x" aria-hidden="true" link="https://symmetricstrength.com/lifter/mechdriver"/></div> {/*Symmetric Strength*/}
+          <div className="pure-u-1-2 pure-u-md-1-2 pure-u-lg-1-3"><InterestBox name="Spotify" iconClass="fab fa-spotify fa-5x" aria-hidden="true" link="https://open.spotify.com/user/mechdriver"/></div> {/*Spotify Account*/}
         </div>
       </div>
     );
